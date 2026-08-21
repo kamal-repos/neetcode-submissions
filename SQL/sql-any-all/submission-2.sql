cc681b8e51ea1e36fe5dd014f1eb8e673671ba34
Kamalakar Reddy Bounapalli <bounapallikamalakar@gmail.com>
2026-08-21 15:16:56 -0500

    Add: sql-any-all - submission-2

---
+++ SQL/sql-any-all/submission-2.sql
@@ -0,0 +1,45 @@
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name TEXT
+);
+
+CREATE TABLE orders (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    customer_id INTEGER,
+    price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 300),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+
+SELECT c.name FROM customers c
+WHERE EXISTS (
+  SELECT 1 FROM orders o WHERE c.id = o.customer_id AND o.price < 100
+)
+ORDER BY c.name ASC;
+
+
+
+
+
+
+
