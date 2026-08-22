3bf33168bda1af223465a453aab1d1afcb41ccdf
Kamalakar Reddy Bounapalli <bounapallikamalakar@gmail.com>
2026-08-21 19:15:00 -0500

    Add: sql-customers-bought-a-b-not-c - submission-0

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-0.sql
@@ -0,0 +1,8 @@
+-- Write your query below
+
+SELECT c.customer_id, c.customer_name
+FROM customers c
+WHERE EXISTS (SELECT 1 FROM orders o WHERE o.customer_id = c.customer_id and o.product_name = 'A')
+AND EXISTS (SELECT 1 FROM orders o WHERE o.customer_id = c.customer_id and o.product_name = 'B')
+AND NOT EXISTS (SELECT 1 FROM orders o WHERE o.customer_id = c.customer_id and o.product_name = 'C')
+ORDER BY c.customer_name;
