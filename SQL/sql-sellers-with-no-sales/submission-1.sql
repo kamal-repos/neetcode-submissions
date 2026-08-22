e2f816185ff48670e390e1f4e1555589090cbdd9
Kamalakar Reddy Bounapalli <bounapallikamalakar@gmail.com>
2026-08-22 13:48:58 -0500

    Add: sql-sellers-with-no-sales - submission-1

---
+++ SQL/sql-sellers-with-no-sales/submission-1.sql
@@ -0,0 +1,14 @@
+-- Write your query below
+-- SELECT s.seller_name
+-- FROM seller s
+-- WHERE NOT EXISTS (
+--     SELECT 1 FROM orders o WHERE s.seller_id = o.seller_id AND EXTRACT(YEAR FROM o.sale_date) = '2020'
+-- )
+-- ORDER BY s.seller_name ASC;
+
+SELECT s.seller_name
+FROM seller s
+WHERE s.seller_id NOT IN (
+    SELECT o.seller_id FROM orders o where EXTRACT(YEAR FROM o.sale_date) = '2020'
+)
+ORDER BY s.seller_name ASC;
