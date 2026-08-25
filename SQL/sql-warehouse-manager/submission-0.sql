828793bab0cef346bae8a8e134cf473d18f29127
Kamalakar Reddy Bounapalli <bounapallikamalakar@gmail.com>
2026-08-24 21:36:38 -0500

    Add: sql-warehouse-manager - submission-0

---
+++ SQL/sql-warehouse-manager/submission-0.sql
@@ -0,0 +1,8 @@
+-- Write your query below
+
+
+SELECT w.name AS warehouse_name, 
+       SUM(w.units * p.width * p.length * p.height) AS volume 
+FROM warehouse w
+LEFT JOIN products p ON w.product_id = p.product_id
+GROUP BY w.name;
