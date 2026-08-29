776ad11ccd3bc51aff15f752b51d344597e0b188
Kamalakar Reddy Bounapalli <bounapallikamalakar@gmail.com>
2026-08-28 22:48:58 -0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] array = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            array[s.charAt(i) - 'a']++;
+            array[t.charAt(i) - 'a']--;
+        }
+
+        for (int i : array) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
